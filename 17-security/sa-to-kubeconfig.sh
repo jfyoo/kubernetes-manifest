776820931851c95d70ac9dfbffcd7b83055eba9b
jfyoo <moujf888@gmail.com>
2024-04-11 14:08:07 +0800

    add kubernetes-manifest

---
+++ 17-security/sa-to-kubeconfig.sh
@@ -0,0 +1,41 @@
+# The script returns a kubeconfig for the service account given
+# you need to have kubectl on PATH with the context set to the cluster you want to create the config for
+
+# Cosmetics for the created config
+clusterName=kubernetes
+# your server address goes here get it via `kubectl cluster-info`
+server=https://172.17.0.93:6443
+# the Namespace and ServiceAccount name that is used for the config
+namespace=default
+serviceAccount=cs-admin
+
+######################
+# actual script starts
+set -o errexit
+
+secretName=$(kubectl --namespace $namespace get serviceAccount $serviceAccount -o jsonpath='{.secrets[0].name}')
+ca=$(kubectl --namespace $namespace get secret/$secretName -o jsonpath='{.data.ca\.crt}')
+token=$(kubectl --namespace $namespace get secret/$secretName -o jsonpath='{.data.token}' | base64 --decode)
+
+echo "
+---
+apiVersion: v1
+kind: Config
+clusters:
+  - name: ${clusterName}
+    cluster:
+      certificate-authority-data: ${ca}
+      server: ${server}
+contexts:
+  - name: ${serviceAccount}@${clusterName}
+    context:
+      cluster: ${clusterName}
+      namespace: ${namespace}
+      user: ${serviceAccount}
+users:
+  - name: ${serviceAccount}
+    user:
+      token: ${token}
+current-context: ${serviceAccount}@${clusterName}
+"
+
